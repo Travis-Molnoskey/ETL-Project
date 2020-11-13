5d6e1e765ad91d684816c2f206ea191407cacf6c
Travis Molnoskey <t.molnoskey@gmail.com>
2020-11-13 00:18:54 -0800

    adding final files with data cleanup

--- queries.sql
+++ queries.sql
@@ -1,25 +1,9 @@
--- Create tables for raw data to be loaded into
-CREATE TABLE customer_name (
-id INT PRIMARY KEY,
-first_name TEXT,
-last_name TEXT
-);
 
-CREATE TABLE customer_location (
-id INT PRIMARY KEY,
-address TEXT,
-us_state TEXT
-);
-
-
--- Joins tables
-SELECT customer_name.id, customer_name.first_name, customer_name.last_name, customer_location.address, customer_location.us_state
-FROM customer_name
-JOIN customer_location
-ON customer_name.id = customer_location.id;
 ----------------------------------------------------------------
+DROP TABLE IF EXISTS listing CASCADE;
+
 CREATE TABLE listing (
-id INT PRIMARY KEY,
+listing_id INT PRIMARY KEY,
 country_id INT,
 city_id INT,
 listing_name VARCHAR, 
@@ -28,40 +12,44 @@ location_name VARCHAR,
 host_name VARCHAR, 
 capacity INT, 
 bedrooms float,
-beds INT,
+beds float,
 price float, 
 price_weekly float, 
 price_montly float, 
 deposit float,
 cleaning_fee float, 
-min_nights INT,
-max_nights INT, 
-number_reviews INT, 
-review_rating INT, 
-listing_url VARCHAR, 
+minimum_nights INT,
+maximum_nights INT, 
+number_of_reviews INT, 
+review_rating float, 
+listing_url VARCHAR
 );
+
+
+DROP TABLE IF EXISTS reviews CASCADE;
 
 CREATE TABLE reviews (
 id INT PRIMARY KEY ,
 listing_id VARCHAR, 
 date VARCHAR, 
-comment_id VARCHAR
+comments VARCHAR
 );
+
+
+DROP TABLE IF EXISTS city CASCADE;
 
 CREATE TABLE city (
 id INT PRIMARY KEY,
-listing_id VARCHAR,
 city_name VARCHAR,
-country_name VARCHAR,
-population INT,
-GDP_per_capita FLOAT
+country_id INT,
+population INT
 );
 
 
+DROP TABLE IF EXISTS country CASCADE;
+
 CREATE TABLE country(
 id INT PRIMARY KEY,
-listing_id VARCHAR,
-country_id VARCHAR,
 country_name VARCHAR,
 population INT,
 GDP_per_capita FLOAT
